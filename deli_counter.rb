d1591eb8437e64eae71c9e8cd423cb9cdd446f47
DAZUN XING <dazun.xing@gmail.com>
2020-04-06 00:59:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ katz_deli = []
 
 def take_a_number(array, name)
   array.push(name)
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  position = array.index {|x| x==name}
+  puts "Welcome, #{name}. You are number #{array.index {|x| x==name}+1} in line."
   return name, position 
 end 
 
